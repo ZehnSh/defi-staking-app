2b56f8c205c4a20ff3b6afd4d159988b0b8e99d2
ZehnSh <jenith281998@gmail.com>
2021-11-03 12:27:47 +0530

    Updates

---
+++ precache-manifest.f61db3e3e422fe5229f3ad9e89f9cc05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e13003181c6c059c8f4",
+    "url": "/defi-staking-app/static/css/main.45bc670a.chunk.css"
+  },
+  {
+    "revision": "7e13003181c6c059c8f4",
+    "url": "/defi-staking-app/static/js/main.7e130031.chunk.js"
+  },
+  {
+    "revision": "37e5a95d96efa202533a",
+    "url": "/defi-staking-app/static/css/1.6941fd64.chunk.css"
+  },
+  {
+    "revision": "37e5a95d96efa202533a",
+    "url": "/defi-staking-app/static/js/1.37e5a95d.chunk.js"
+  },
+  {
+    "revision": "7145792dd01e6798ba67",
+    "url": "/defi-staking-app/static/js/runtime~main.7145792d.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/defi-staking-app/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "ec2ea4f58a4c69ddc18df835a8502473",
+    "url": "/defi-staking-app/index.html"
+  }
+];
